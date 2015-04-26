dbbd638346f6b6cfcc11bdd8a357c44a3cb6ea6a
Paul Grever <paulgrever@gmail.com>
2015-04-26 16:00:21 -0600

    hound fixes

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,14 +2,12 @@ require "rails_helper"
 
 RSpec.describe Organization, type: :model do
   # context "with all the correct credentials" do
-  #   before(:each) do 
+  #   before(:each) do
   #     VCR.use_cassette("organization_validate") do
   #       @data = OrganizationVerifier.new(142007220)
-  #     end 
+  #     end
   #     organization = Organization.create(@data.parse_response)
   #   end
-
-    
 
   #   it "has a name" do
 
