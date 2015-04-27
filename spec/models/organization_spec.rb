80263315aef1965759bbe43528bd5a98c9803976
Paul Grever <paulgrever@gmail.com>
2015-04-27 17:18:19 -0600

    removed in progrss test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Organization, type: :model do
-  VCR.use_cassette("model_organization") do 
+  # VCR.use_cassette("model_organization") do 
     
-      @organization = create(:organization)
-      OrganizationVerifier.new(@organization.ein)
+  #     @organization = create(:organization)
+  #     OrganizationVerifier.new(@organization.ein)
 
-    it "has a name" do
-      expect(@organization.name).to eq("PRO PUBLICA INC")
-    end
-  end
+  #   it "has a name" do
+  #     expect(@organization.name).to eq("PRO PUBLICA INC")
+  #   end
+  # end
 end
