18f876eb7f9ee3cff3b7add115bf6d83e9a8090c
Paul Grever <paulgrever@gmail.com>
2015-04-27 22:59:20 -0600

    hound fixes

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,32 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Organization, type: :model do
-  context "with valid information"
-    before(:each)do
-      VCR.use_cassette("model_organization") do
-          @organization = create(:organization)
-          @parsed_organization = OrganizationVerifier.new(@organization.ein).parse_response
-      end
-      it "has a name" do
-        expect(@parsed_organization.name).to eq("PRO PUBLICA INC")
-      end
 
-      it "verifies if it's an valid ein" do
-        expect(@organization.valid_ein).to eq(true)
-      end
-
-      it "returns false if there are no events associated with an organization" do
-        expect(@parsed_organization.any_events?).to eq(false)
-      end
+  before(:each) do
+    VCR.use_cassette("model_organization") do
+        @organization = create(:organization)
+        @parsed_organization = OrganizationVerifier.new(@organization.ein).parse_response
+    end
   end
 
-  # context "with invalid information" do
-  #   it "is invalid with an invalid ein " do
-  #     VCR.use_cassette("model_organization") do
-  #       @organization = create(:organization, ein: 9900)
-  #       @parsed_organization = OrganizationVerifier.new(@organization.ein).parse_response
-  #     end
-  #   end
-   
-  # end
+  it "has a name" do
+    expect(@parsed_organization[:name]).to eq("PRO PUBLICA INC")
+  end
+
+  it "verifies if it's an valid ein" do
+    VCR.use_cassette("valid_ein") do
+        @organization = create(:organization, ein: 10563036)
+        @parsed_organization = OrganizationVerifier.new(@organization.ein).parse_response
+    end
+    VCR.use_cassette("valid_ein") do
+      expect(@organization.valid_ein).to eq(true)
+    end
+  end
+
+  it "returns false if there are no events associated with an organization" do
+    expect(@organization.any_events?).to eq(false)
+  end
 end
