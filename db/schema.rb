c333fa378b5d651dfc309cba8f3b50028eea818b
Paul Grever <paulgrever@gmail.com>
2015-04-27 22:44:56 -0600

    events migration to dispay all fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428004444) do
+ActiveRecord::Schema.define(version: 20150428040808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,11 @@ ActiveRecord::Schema.define(version: 20150428004444) do
     t.float    "latitude"
     t.float    "longitude"
     t.integer  "organization_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                                                  null: false
+    t.datetime "updated_at",                                                  null: false
+    t.string   "short_description"
+    t.text     "long_description"
+    t.string   "image_url",         default: "socialteering_placeholder.png"
   end
 
   create_table "organizations", force: :cascade do |t|
--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -11,4 +11,17 @@ FactoryGirl.define do
     organization_type "A20"
     user
   end
+
+  factory :incomplete_organization, class: Organization do
+    ein 142007220
+    name "PRO PUBLICA INC"
+    stein "142007220"
+    careofname "Paul"
+    address "ONE EXCHANGE PLZ 55 BROADWAY 23 FL"
+    city "NEW YORK"
+    state "NY"
+    zipcode "10006-0000"
+    organization_type "A20"
+    user
+  end
 end
